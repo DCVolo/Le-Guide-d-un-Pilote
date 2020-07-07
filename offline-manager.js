6bae8d91ca2e96c5bae5f4cb8f84316451de1e48
Volodia MANDAUD <llvolo2@gmail.com>
2020-07-07 14:44:11 +0200

    pwa

--- offline-manager.js
+++ offline-manager.js
@@ -16,7 +16,7 @@ Copyright 2015, 2019 Google Inc. All Rights Reserved.
 const OFFLINE_VERSION = 1;
 const CACHE_NAME = 'offline-LGDP';
 // Customize this with a different URL if needed.
-const OFFLINE_URL = 'index.html';
+const OFFLINE_URL = '.';
 // Precaching critical static files
 const STATIC_CACHE_URLS = ["Le-Guide-d-un-Pilote.pdf"];
 
