bdf2de10d9212849f64e49a25e86107b075a0868
Volodia MANDAUD <llvolo2@gmail.com>
2020-07-07 02:05:32 +0200

    pwa

--- sw-install-pwa-prompt.js
+++ sw-install-pwa-prompt.js
@@ -1,36 +1,31 @@
-// Code to handle install prompt on desktop
-let deferredPrompt;
-//const addBtn = document.querySelector('.installAppPWA');
-const addBtn = document.getElementByClassName('installAppPWA');
+let deferredPrompt; // Allows to show the install prompt
+const installButton = document.getElementsByClassName("install_button")[0];
 
-
-	document.getElementsByClassName('installAppPWA')[0].addEventListener("click", function(){
-  		deferredPrompt.prompt();
-  		console.log('tentative prompt');
-	});
-
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
+window.addEventListener("beforeinstallprompt", e => {
+  console.log("beforeinstallprompt fired");
+  // Prevent Chrome 76 and earlier from automatically showing a prompt
   e.preventDefault();
   // Stash the event so it can be triggered later.
   deferredPrompt = e;
-  // Update UI to notify the user they can add to home screen
-  addBtn.style.display = 'block';
-
-  addBtn.addEventListener('click', (e) => {
-    // hide our user interface that shows our A2HS button
-    addBtn.style.display = 'none';
-    // Show the prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the A2HS prompt');
-        } else {
-          console.log('User dismissed the A2HS prompt');
-        }
-        deferredPrompt = null;
-      });
-  });
+  // Show the install button
+  installButton.hidden = false;
+  installButton.addEventListener("click", installApp);
 });
+
+windows.showInstallPrompt = function (){
+	// Show the prompt
+  	deferredPrompt.prompt();
+  	installButton.disabled = true;
+
+  	// Wait for the user to respond to the prompt
+  	deferredPrompt.userChoice.then(choiceResult => {
+    	if (choiceResult.outcome === "accepted") {
+      		console.log("PWA setup accepted");
+      		installButton.hidden = true;
+    	} else {
+      		console.log("PWA setup rejected");
+    	}
+    	installButton.disabled = false;
+    	deferredPrompt = null;
+  	});
+}
